a45e10490b71315fa1384f7198995e5b1e8043d8
Wybren Oppedijk <wybren11@live.nl>
2015-11-04 18:48:15 +0100

    Initial Commit

---
+++ helpDesk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helpDesk
+//
+//  Created by Thuis on 04-11-15.
+//  Copyright © 2015 Amerion IT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
