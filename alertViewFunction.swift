427b311b73c070c2e4cfd6083e7c50c3377bbf96
Erik <eriklaan2@gmail.com>
2016-01-08 13:58:57 +0100

    Simplified alert messages, replaced all with public function.

---
+++ alertViewFunction.swift
@@ -0,0 +1,20 @@
+//
+//  alertViewFunction.swift
+//  helpDesk
+//
+//  Created by Wybren Oppedijk on 08-01-16.
+//  Copyright © 2016 Amerion IT. All rights reserved.
+//
+
+import Foundation
+
+public class alertViewFunction {
+    
+    public func create(title: String, message: String) -> UIViewController {
+        let alertView:UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in }
+        alertView.addAction(OKAction)
+        return alertView
+    }
+    
+}
--- changePasswordViewController.swift
+++ changePasswordViewController.swift
@@ -59,64 +59,6 @@ class changePasswordViewController: UIViewController {
     }
     
     internal func changePasswordInDatabase(username: NSString, password: NSString) -> Void {
-        do {
-            let post:NSString = "username=\(username)&password=\(password)"
-            
-            let url:NSURL = NSURL(string:dbURL)!
-            
-            let postData:NSData = post.dataUsingEncoding(NSASCIIStringEncoding)!
-            
-            let postLength:NSString = String( postData.length )
-            
-            let request:NSMutableURLRequest = NSMutableURLRequest(URL: url)
-            request.HTTPMethod = "POST"
-            request.HTTPBody = postData
-            request.setValue(postLength as String, forHTTPHeaderField: "Content-Length")
-            request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-            request.setValue("application/json", forHTTPHeaderField: "Accept")
-            
-            var response: NSURLResponse?
-            
-            var urlData: NSData?
-            do {
-                urlData = try NSURLConnection.sendSynchronousRequest(request, returningResponse:&response) // Deprecated
-            
-                if ( urlData != nil ) {
-                    let res = response as! NSHTTPURLResponse!;
-                
-                    if (res.statusCode >= 200 && res.statusCode < 300) {
-                        let jsonData:NSDictionary = try NSJSONSerialization.JSONObjectWithData(urlData!, options:NSJSONReadingOptions.MutableContainers ) as! NSDictionary
-                        let success:NSInteger = jsonData.valueForKey("succes") as! NSInteger
-                        if(success == 1) {
-                            let alertView:UIAlertController = UIAlertController(title: "Wachtwoord gewijzigd", message: "Uw wachtwoord is gewijzigd.", preferredStyle: .Alert)
-                            let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in }
-                            alertView.addAction(OKAction)
-                            self.presentViewController(alertView, animated: true, completion: nil)
-                        } else {
-                            let alertView:UIAlertController = UIAlertController(title: "Wachtwoord niet gewijzigd", message: "Er is een fout opgetreden (foutcode i10).", preferredStyle: .Alert)
-                            let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in }
-                            alertView.addAction(OKAction)
-                            self.presentViewController(alertView, animated: true, completion: nil)
-                        }
-                    } else {
-                        let alertView:UIAlertController = UIAlertController(title: "Wachtwoord niet gewijzigd", message: "Er is een fout opgetreden (foutcode i07).", preferredStyle: .Alert)
-                        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in }
-                        alertView.addAction(OKAction)
-                        self.presentViewController(alertView, animated: true, completion: nil)
-                    }
-                } else {
-                    let alertView:UIAlertController = UIAlertController(title: "Wachtwoord niet gewijzigd", message: "Er is een fout opgetreden (foutcode i08).", preferredStyle: .Alert)
-                    let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in }
-                    alertView.addAction(OKAction)
-                    self.presentViewController(alertView, animated: true, completion: nil)            }
-            } catch {
-                let alertView:UIAlertController = UIAlertController(title: "Wachtwoord niet gewijzigd", message: "Er is een fout opgetreden (foutcode i09).", preferredStyle: .Alert)
-                let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in }
-                alertView.addAction(OKAction)
-                self.presentViewController(alertView, animated: true, completion: nil)
-            }
-
-        }
-    
+        // TODO: This function
     }
 }
